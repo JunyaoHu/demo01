faf12cec964f94ff492f61683154351a07c8fee3
JunyaoHu <1078622540@qq.com>
2022-01-02 12:29:21 +0800

    Initial commit

---
+++ src/demo01.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class demo01 {
+    public static int str2num(StringBuffer strBuf) {
+        int res = 0;
+        for (int i = 0; i < strBuf.length(); i++) {
+            int tmp = strBuf.charAt(i) - '0';
+            res *= 10;
+            res += tmp;
+        }
+        return res;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = "";
+        try {
+            str = sc.next();
+        } catch (Exception e) {
+            System.out.println("Error!");
+        }
+
+        StringBuffer number = new StringBuffer();
+        StringBuffer upper = new StringBuffer();
+        StringBuffer lower = new StringBuffer();
+
+        int i = 0;
+        char ch;
+        while (i < str.length()) {
+            ch = str.charAt(i);
+            if('0' <= ch && ch <= '9') {
+                do {
+                    number.append(ch);
+                    i++;
+                    if(i < str.length()) ch=str.charAt(i);
+                    else break;
+                } while('0' <= ch && ch <= '9');
+                int num = str2num(number);
+                System.out.println(num);
+                number = new StringBuffer();
+                continue;
+            }
+
+            else if ('A' <= ch && ch <= 'Z') {
+                do {
+                    upper.append(ch);
+                    i++;
+                    if(i < str.length()) ch=str.charAt(i);
+                    else break;
+                } while ('A' <= ch && ch <= 'Z');
+                System.out.println(upper);
+                upper = new StringBuffer();
+                continue;
+            }
+
+            else if ('a' <= ch && ch <= 'z') {
+                do {
+                    lower.append(ch);
+                    i++;
+                    if(i < str.length()) ch=str.charAt(i);
+                    else break;
+                }while('a' <= ch && ch <= 'z');
+                System.out.println(lower);
+                lower = new StringBuffer();
+                continue;
+            }
+        }
+    }
+
+
+}
